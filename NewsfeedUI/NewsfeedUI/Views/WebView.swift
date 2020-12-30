b3dd727f5ec4aadaf7dd3c2781e126089dee23cb
Merlin Levine <merlin.levine@capitalone.com>
2020-12-30 16:06:18 -0500

    added detail view

---
+++ NewsfeedUI/NewsfeedUI/Views/WebView.swift
@@ -0,0 +1,20 @@
+//
+//  WebView.swift
+//  NewsfeedUI
+//
+//  Created by Levine, Merlin on 12/30/20.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
