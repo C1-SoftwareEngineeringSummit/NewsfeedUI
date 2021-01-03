a79b5757c96af4a64a4f7ca36e5e06c93141289a
Chris Longe <chris.longe@capitalone.com>
2021-01-03 14:32:01 -0500

    Base project with modified bundle identifier.

---
+++ NewsfeedUI-Starter/NewsfeedUI-Starter/NewsfeedUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsfeedUI_StarterApp.swift
+//  NewsfeedUI-Starter
+//
+//  Created by Longe, Chris on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsfeedUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
