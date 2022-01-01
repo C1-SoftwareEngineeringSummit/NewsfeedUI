7679b55b61b4b43d94f63117126122721c3d74af
Merlin Levine <merlin.levine@capitalone.com>
2022-01-01 15:12:09 -0500

    added list style to contentview, updated deployment target to 15.0, updated instructions

--- NewsfeedUI/NewsfeedUI/Views/ContentView.swift
+++ NewsfeedUI/NewsfeedUI/Views/ContentView.swift
@@ -48,6 +48,7 @@ struct ContentView: View {
                         .listRowInsets(EdgeInsets())
                 }
             }
+            .listStyle(PlainListStyle())
             .navigationTitle("Newsfeed")
         }
     }
